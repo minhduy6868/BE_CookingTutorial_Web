9fa6575e0907263d31d2938190e7ddded7391901
minhduy6868 <fbminhduyy4167@gmail.con>
2024-12-08 22:09:36 +0700

    check code

--- src/main/java/com/example/CookingTutorial/config/CloudinaryConfig.java
+++ src/main/java/com/example/CookingTutorial/config/CloudinaryConfig.java
@@ -1,6 +1,7 @@
 package com.example.CookingTutorial.config;
 
 import com.cloudinary.Cloudinary;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
@@ -13,13 +14,14 @@ import java.util.Map;
 @EnableMethodSecurity(prePostEnabled = true)
 @CrossOrigin
 public class CloudinaryConfig {
-
+    @Value("${jwt.cloudySecret}")
+    private String cloudySecret;
     @Bean
     public Cloudinary getCloudinary() {
         Map<String, String> config = new HashMap<>();
         config.put("cloud_name", "dz22gpipd");
         config.put("api_key", "816781279367396");
-        config.put("api_secret", "4Yct2Y7U64h2DlUbtEm0ctRvhIM");
+        config.put("api_secret", cloudySecret );
         config.put("secure", "true");
         return new Cloudinary(config);
     }
